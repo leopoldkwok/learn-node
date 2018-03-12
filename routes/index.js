928e5a728d92fa08e211bc74e9072eed98a8657e
Leo <leo.kwok@beamly.com>
2018-03-12 13:53:13 +0000

    Core Concept - Controllers and the MVC Pattern

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const storeController = require('../controllers/storeController')
 // Do work here
-router.get('/', (req, res) => {
-  const wes = { name: 'Wes', age: 100, cool: true };
-  // res.send('Hey! It works!');
-  // res.send(wes);
-  // res.send(req.query.name);
-  // res.json(req.query);
-  res.render('hello', {
-    name: 'wes',
-    dog: req.query.dog,
-    title: 'I love food'
-   });
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join();
-  res.send(req.params.name);
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
